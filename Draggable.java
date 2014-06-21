4130c4844d4aa3ee44586b717bfcdf172e3d384e
Pithon3 <90303158@ep-student.org>
2014-06-20 19:48:33 -0500

    Fix Compile error
    small bug that prevents it from being compiled

--- Draggable.java
+++ Draggable.java
@@ -11,6 +11,7 @@ public class Draggable extends Sprite{
 	boolean mousein = false;  //boolean variable that represents if the mouse is in the area of the sprite
 	
 	Good1 master;  //the master of this sprite
+	public String filepath;
 	
 	public Draggable(String filepath, int x, int y, int xlong, int ylong, String n, Good1 good1) {
 		super(filepath, x, y, xlong, ylong);
